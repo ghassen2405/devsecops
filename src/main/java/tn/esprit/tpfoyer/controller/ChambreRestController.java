77d1c27cda5775142bb7676dad55b19c614a2977
ghassen2405 <dridighassen05@gmail.com>
2025-11-13 01:13:39 +0100

    xxx

--- src/main/java/tn/esprit/tpfoyer/controller/ChambreRestController.java
+++ src/main/java/tn/esprit/tpfoyer/controller/ChambreRestController.java
@@ -17,33 +17,34 @@ import java.util.List;
 public class ChambreRestController {
     @Autowired
     IChambreService chambreService;
-
+    // http://localhost:8089/tpfoyer/chambre/retrieve-all-chambres
     @Operation(description = "récupérer toutes les chambres de la base de données")
     @GetMapping("/retrieve-all-chambres")
     public List<Chambre> getChambres() {
         List<Chambre> listChambres = chambreService.retrieveAllChambres();
         return listChambres;
     }
-
+    // http://localhost:8089/tpfoyer/chambre/retrieve-chambre/8
     @GetMapping("/retrieve-chambre/{chambre-id}")
     public Chambre retrieveChambre(@PathVariable("chambre-id") Long chId) {
         Chambre chambre = chambreService.retrieveChambre(chId);
         return chambre;
     }
 
+    // http://localhost:8089/tpfoyer/chambre/add-chambre
     @PostMapping("/add-chambre")
     public Chambre addChambre(@RequestBody Chambre c) {
         Chambre chambre = chambreService.addChambre(c);
         return chambre;
-        return chambre;
     }
-
+    // http://localhost:8089/tpfoyer/chambre/remove-chambre/{chambre-id}
     @DeleteMapping("/remove-chambre/{chambre-id}")
-        chambreService.removeChambre(chId);
+    public void removeChambre(@PathVariable("chambre-id") Long chId) {
         chambreService.removeChambre(chId);
     }
-
+    // http://localhost:8089/tpfoyer/chambre/modify-chambre
     @PutMapping("/modify-chambre")
+    public Chambre modifyChambre(@RequestBody Chambre c) {
         Chambre chambre = chambreService.modifyChambre(c);
         return chambre;
     }
